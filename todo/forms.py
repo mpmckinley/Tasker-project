7a2c0bab9d87fc743f4ce61a3961fc61bd3b4a71
mpmckinley <mikepmckinley@gmail.com>
2020-12-19 13:15:06 -0700

    Form

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,21 @@
-from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+from django.forms import ModelForm, widgets
 from .models import Todo
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'memo', 'important', 'duedate']
+        labels = {
+            'title': "Task"
+        }
+        help_texts = {
+            'title': "This is a helpful thing."
+        }
+        error_messages = {
+            'title': {
+                'max_length': _("The title is too long."),
+            },
+        }
+        widgets = {
+        }
